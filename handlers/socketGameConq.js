0bf5fec8c34a2de3a3f08e952af1e68388e4be65
rovingiene <rovingiene@gmail.com>
2022-07-22 20:25:51 +0800

    add countdown

--- handlers/socketGameConq.js
+++ handlers/socketGameConq.js
@@ -1,15 +1,32 @@
 import events from "../data/socketEvents.js";
 import { onMessage, sendMessage } from "../utils/socketUtils.js";
 
+var gameMap = new Map();
+var timerMap = new Map();
+var queueMap = new Map();
+
 const socketGameConq = (socket, io) => {
   var playing = 0;
   const status = {
     players: io.sockets.sockets.size,
     playing: playing,
   };
+
   playing += 1;
 
   sendMessage(socket, events.status, status);
+
+  onMessage(socket, "start_countdown", () => {
+    console.log("counting down");
+    let countdownTimer = 10;
+    let countdown = setInterval(() => {
+      countdownTimer -= 1;
+      sendMessage(socket, "countdown", countdownTimer);
+      if (countdownTimer === 0) {
+        clearInterval(countdown);
+      }
+    }, 1000);
+  });
 
   onMessage(socket, events.move, (res) => {
     console.log(res);
